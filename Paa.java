d6fc9471d06b191b585fb8009b6e1c9202abea44
Andika Nugraha <m.andika.nugraha@gmail.com>
2017-10-04 19:29:59 +0700

    pert 1

---
+++ Paa.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Paa {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int nRequest;
+        // List<Request> listRequest = new ArrayList<~>();
+        System.out.print("Interval Scheduling");    
+    }
+}
